0dbad530ac469bfbe39f88e89f2944fc086ccdf9
Nicholas Lim <weishou.nick@gmail.com>
2021-02-17 10:32:23 +0800

    go

--- FST/fst_proximate.R
+++ FST/fst_proximate.R
@@ -9,7 +9,8 @@ fst_data<-read_excel(here("FST", "fst_proximate.xlsx"), sheet="Sheet1")
 ggplot(data=fst_data, aes(y=Mean, x=Line, group=Line, colour=Line))+
   geom_pointrange(aes(ymin=Mean-SD, ymax=Mean+SD))+
   facet_wrap(~Parameters, scales='free')+
-  theme(text = element_text(size=20))
+  theme(text = element_text(size=20))+
+  labs(y="Mean %")
 
 
 
