62c2936c62b9abb78dddf65ae9a6b5422b61be17
Nicholas Lim <weishou.nick@gmail.com>
2021-02-17 10:28:52 +0800

    text size

---
+++ FST/fst_proximate.R
@@ -0,0 +1,15 @@
+library(here)
+library(readxl)
+library(ggplot2)
+here()
+
+#Feed in data
+fst_data<-read_excel(here("FST", "fst_proximate.xlsx"), sheet="Sheet1")
+
+ggplot(data=fst_data, aes(y=Mean, x=Line, group=Line, colour=Line))+
+  geom_pointrange(aes(ymin=Mean-SD, ymax=Mean+SD))+
+  facet_wrap(~Parameters, scales='free')+
+  theme(text = element_text(size=20))
+
+
+
